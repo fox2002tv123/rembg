c88009b910676eee548feac38b9426930d59b041
fox2002tv123 <85301925@qq.com>
2022-04-13 17:08:37 +0800

    test_app中添加了备注

--- test_app.py
+++ test_app.py
@@ -2,14 +2,14 @@
 Author: bob
 Date: 2022-04-13 16:39:10
 LastEditors: bob
-LastEditTime: 2022-04-13 16:59:19
+LastEditTime: 2022-04-13 17:08:06
 FilePath: \rembg\test_app.py
 Description: 测试程序
 
 Copyright (c) 2022 by bob, All Rights Reserved. 
 '''
 from rembg import remove # 导入程序
-from PIL import Image
+from PIL import Image # 导入PIL库
 
 input_path = 'input.jpg' # 输入图片路径
 output_path = 'output.png' # 输出图片路径
